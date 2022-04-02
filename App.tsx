48c8d7c52f3ca6061eac216f0dca2d71a5e44df8
John <ewertonjohn8@gmail.com>
2022-04-02 18:37:08 -0300

    feat: adicionando imagem da logo do leva+

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello Friend!</Text>
       <StatusBar style="auto" />
+      <Image 
+      resizeMode={'contain'}
+      style={styles.image}
+      source={require('./assets/logo_leva+.png')}
+      //style={{ width: 50, height: 50 }}
+      />  
     </View>
   );
 }
@@ -13,8 +19,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#121212',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    width: 586,
+    height: 159
+  }
+
 });
