242750c268b243ddb29333ce870e6550f45ebacc
John <ewertonjohn8@gmail.com>
2022-04-13 16:17:33 -0300

    feat: adicionando componente default

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Friend!</Text>
+      <LinearGradient
+          colors={['rgba(31, 125, 188, 1)', 'transparent']}
+          style={styles.background}
+       />
       <StatusBar style="auto" />
       <Image 
-      resizeMode={'contain'}
-      style={styles.image}
-      source={require('./assets/logo_leva+.png')}
-      //style={{ width: 50, height: 50 }}
+        resizeMode={'contain'}
+        style={styles.image}
+        source={require('./assets/logo.png')}
       />  
     </View>
   );
@@ -19,13 +23,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#121212',
+    //background: 'linear-gradient(180deg, #1F7DBC 0%, #56CBF2 100%)',
+    //'rgba(86, 203, 242, 1)', 'rgba(86, 203, 242, 1)', 'transparent'
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: 'rgba(86, 203, 242, 1)'
   },
   image: {
-    width: 586,
-    height: 159
+    width: 300,
+    height: 80
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: 300
   }
 
 });
