c6fc16c4a5bb67464c26480a6828343235e3d357
John <ewertonjohn8@gmail.com>
2022-07-21 22:00:33 -0300

    feat: navegation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+import { Button, color } from '@rneui/base';
+import React from 'react';
 
+const Separator = () => (
+  <View style={styles.separator} />
+);
 
-export default function App() {
-  return (
+const Stack = createStackNavigator()
+
+function HomeScreen({navigation}: any) {
+  return(
+    <SafeAreaView style={styles.container}>
+      <LinearGradient
+          colors={['rgba(31, 125, 188, 1)', 'transparent']}
+          style={styles.background}
+       />
+      <StatusBar style="auto" />
+      <View>
+        <Image 
+          resizeMode={'contain'}
+          style={styles.image}
+          source={require('./assets/logo.png')}
+        />
+      </View>
+      <Separator />
+      <View>
+        <Button
+          style={styles.Button}
+          title="Cadastro"
+          onPress={() => navigation.navigate('Cadastro')}
+        />  
+      </View>
+    </SafeAreaView>
+  );
+}
+
+function Form({navigation}: any) {
+  return(
     <View style={styles.container}>
       <LinearGradient
           colors={['rgba(31, 125, 188, 1)', 'transparent']}
@@ -15,8 +52,30 @@ export default function App() {
         resizeMode={'contain'}
         style={styles.image}
         source={require('./assets/logo.png')}
-      />  
+      />
     </View>
+  );
+}
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}
+        options={{
+          headerTransparent: true,
+          headerTitleAlign: 'center',
+          headerTitleStyle: {color: 'white'}
+        }} />
+        <Stack.Screen name='Cadastro' component={Form}
+        options={{
+          headerTransparent: true,
+          headerTitleAlign: 'center',
+          headerTitleStyle: {color: 'white'}
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -39,6 +98,15 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
     height: 300
-  }
+  },
+  Button: {
+    // flexDirection: "row",
+    // justifyContent: "space-between"
+  },
+  separator: {
+    marginVertical: 50,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
 
 });
